736bfe312698695e43a17db5eee6b7ae5fabcced
chyzh-i <chyzh-i@Ubuntu22.04.myguest.virtualbox.org>
2023-02-25 17:30:16 +0200

    fixed_task_4

--- HW_2/task_4.py
+++ HW_2/task_4.py
@@ -1,4 +1,4 @@
 omnivores = ["Lucy", "Diego", "Emanuel"]
-vegeterians = ["Fatima", "Amir", "Michaela"]
-list_of_guests = omnivores + vegeterians
-print(list_of_guests)
+vegetarians = ["Fatima", "Amir", "Michaela"]
+vegetarians.extend(omnivores)
+print(vegetarians)
