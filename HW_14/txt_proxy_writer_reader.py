04aa3c71e4407c9e28fa32e6b3e097f773f02925
chyzh-i <chyzh-i@Ubuntu22.04.myguest.virtualbox.org>
2023-03-11 19:40:00 +0200

    task_from_HW_14_done

---
+++ HW_14/txt_proxy_writer_reader.py
@@ -0,0 +1,23 @@
+from txt_reader import TxtReader
+from txt_writer import TxtWriter
+
+
+class TxtProxyWriterReader:
+    def __init__(self, file_path):
+        self.__result = ''
+        self.__is_actual = False
+        self.__txt_reader = TxtReader(file_path)
+        self.__txt_writer = TxtWriter(file_path)
+
+    def read_file(self):
+        if self.__is_actual:
+            return self.__result
+        else:
+            self.__result = self.__txt_reader.read()
+            self.__is_actual = True
+            return self.__result
+
+    def write_to_file(self):
+        self.__is_actual = False
+        self.__txt_writer.write(input())
+        return self.read_file()
